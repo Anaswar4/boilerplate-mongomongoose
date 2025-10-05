60d81c19e6b7701a79cbfdad01adc1bcec310af8
Anaswar4 <anaswarpk13@gmail.com>
2025-10-05 17:26:01 +0000

    fixed

--- myApp.js
+++ myApp.js
@@ -58,11 +58,7 @@ const removeManyPeople = (done) => {
   done(null /*, data*/);
 };
 
-const queryChain = (done) => {
-  const foodToSearch = "burrito";
 
-  done(null /*, data*/);
-};
 
 /** **Well Done !!**
 /* You completed these challenges, let's go celebrate !
